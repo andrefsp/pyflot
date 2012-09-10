55a339671598de6352cc719eff6dee0a4b2927c4
andre da palma <andrefsp@gmail.com>
2012-09-11 00:52:34 +0100

    reviewed the installation

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from distutils.core import setup
 setup(name='PyFlot',
     version='1.0',
     description='Python Interface for the known JavaScript Flot librabry',
+    long_description=open('README.rst', 'r').read(),
+    license='MIT',
     author='Andre da Palma',
     author_email='andrefsp@gmail.com',
     url='http://github.com/andrefsp/pyflot',
-    packages=['flot'],
+    packages=['flot', 'flot.templatetags'],
     package_dir={'flot': 'src/flot'},
 )
